9da7a9b96a36b46b782179687dbcac1c5b5e22c6
Ravi <raviagniwanshi@gmail.com>
2023-08-29 00:25:49 +0530

    first

---
+++ src/main/java/in/priyanka/App.java
@@ -0,0 +1,13 @@
+package in.priyanka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
